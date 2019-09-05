4e932e83706cafea42e5773957d52a4a663c9b13
Logan Zhao <zhaolong_chn@163.com>
2019-09-05 14:58:00 +0800

    工程

---
+++ Assets/Scripts/WebTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebTest : MonoBehaviour {
+
+	public Text m_Text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdateText() {
+		if (m_Text != null) {
+			m_Text.text = "Time : " + Time.time;
+		}
+	}
+}
